d0857b3eea6ff10b174f2bc0ec229487ee2429ad
Rongpu Zhou <rongpu.zhou@gmail.com>
2021-03-25 11:54:23 -0700

    .

---
+++ dr9/create_target_catalogs/create_per_tracer_catalogs.sh
@@ -0,0 +1,37 @@
+python create_per_tracer_catalogs.py sv1 ELG south
+python create_per_tracer_catalogs.py sv1 ELG north
+python create_per_tracer_catalogs.py sv1 LRG south
+python create_per_tracer_catalogs.py sv1 LRG north
+python create_per_tracer_catalogs.py sv1 QSO south
+python create_per_tracer_catalogs.py sv1 QSO north
+python create_per_tracer_catalogs.py sv1 BGS_ANY south
+python create_per_tracer_catalogs.py sv1 BGS_ANY north
+
+python create_per_tracer_catalogs.py sv1 ELG south
+python create_per_tracer_catalogs.py sv1 ELG north
+python create_per_tracer_catalogs.py sv1 LRG south
+python create_per_tracer_catalogs.py sv1 LRG north
+python create_per_tracer_catalogs.py sv1 QSO south
+python create_per_tracer_catalogs.py sv1 QSO north
+python create_per_tracer_catalogs.py sv1 BGS_ANY south
+python create_per_tracer_catalogs.py sv1 BGS_ANY north
+
+python create_per_tracer_catalogs.py main ELG south
+python create_per_tracer_catalogs.py main ELG north
+python create_per_tracer_catalogs.py main LRG south
+python create_per_tracer_catalogs.py main LRG north
+python create_per_tracer_catalogs.py main QSO south
+python create_per_tracer_catalogs.py main QSO north
+python create_per_tracer_catalogs.py main BGS_ANY south
+python create_per_tracer_catalogs.py main BGS_ANY north
+
+python create_per_tracer_catalogs.py main ELG south
+python create_per_tracer_catalogs.py main ELG north
+python create_per_tracer_catalogs.py main LRG south
+python create_per_tracer_catalogs.py main LRG north
+python create_per_tracer_catalogs.py main QSO south
+python create_per_tracer_catalogs.py main QSO north
+python create_per_tracer_catalogs.py main BGS_ANY south
+python create_per_tracer_catalogs.py main BGS_ANY north
+
+exit
