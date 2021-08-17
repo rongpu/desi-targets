b08378c5576c74a0a7276ce3bc4d86724789cc23
Rongpu Zhou <rongpu.zhou@gmail.com>
2021-08-17 13:51:16 -0700

    .

---
+++ lrg_xcorr/magnification/count_magnified_lrgs.py
@@ -0,0 +1,120 @@
+from __future__ import division, print_function
+import sys, os, glob, time, warnings, gc
+import numpy as np
+from astropy.table import Table, vstack, hstack, join
+import fitsio
+
+import yaml
+
+pz_magnification = True  # Include the photo-z shift
+ff_factor = True  # Include for the fiberflux factor
+
+min_nobs = 2
+maskbits = [1, 8, 9, 11, 12, 13]
+
+columns = ['TARGETID', 'TYPE', 'RA', 'DEC', 'EBV',
+'FLUX_G', 'FLUX_R', 'FLUX_Z', 'FLUX_W1', 'FIBERFLUX_Z',
+'FLUX_IVAR_G', 'FLUX_IVAR_R', 'FLUX_IVAR_Z', 'FLUX_IVAR_W1',
+'MW_TRANSMISSION_G', 'MW_TRANSMISSION_R', 'MW_TRANSMISSION_Z', 'MW_TRANSMISSION_W1',
+'GAIA_PHOT_G_MEAN_MAG', 'FIBERTOTFLUX_Z',
+'NOBS_G', 'NOBS_R', 'NOBS_Z', 'MASKBITS']
+
+counts = dict()
+
+for field in ['north', 'south']:
+
+    cat = Table(fitsio.read('/Users/rongpu/Documents/Data/lrg_xcorr/magnification/lrg_magnification_{}.fits'.format(field), columns=columns))
+    cat1 = Table(fitsio.read('/Users/rongpu/Documents/Data/lrg_xcorr/magnification/lrg_magnification_{}_fiberflux.fits'.format(field)))
+    cat = hstack([cat, cat1])
+
+    if field=='north':
+        mask_ns = (cat['DEC']>32.375)
+    else:
+        mask_ns = (cat['DEC']<=32.375) | (cat['RA']<104) | (cat['RA']>280)
+    cat = cat[mask_ns]
+
+    mask_quality = np.full(len(cat), True)
+
+    mask_quality &= (cat['FLUX_IVAR_R'] > 0) & (cat['FLUX_R'] > 0)   # ADM quality in r.
+    mask_quality &= (cat['FLUX_IVAR_Z'] > 0) & (cat['FLUX_Z'] > 0) & (cat['FIBERFLUX_Z'] > 0)   # ADM quality in z.
+    mask_quality &= (cat['FLUX_IVAR_W1'] > 0) & (cat['FLUX_W1'] > 0)  # ADM quality in W1.
+
+    mask_quality &= (cat['GAIA_PHOT_G_MEAN_MAG'] == 0) | (cat['GAIA_PHOT_G_MEAN_MAG'] > 18)  # remove bright GAIA sources
+
+    # ADM remove stars with zfibertot < 17.5 that are missing from GAIA.
+    mask_quality &= cat['FIBERTOTFLUX_Z'] < 10**(-0.4*(17.5-22.5))
+
+    # ADM observed in every band.
+    mask_quality &= (cat['NOBS_G'] > 0) & (cat['NOBS_R'] > 0) & (cat['NOBS_Z'] > 0)
+
+    mask_quality &= (cat['NOBS_G']>=min_nobs) & (cat['NOBS_R']>=min_nobs) & (cat['NOBS_Z']>=min_nobs)
+
+    # Apply masks
+    mask_clean = np.ones(len(cat), dtype=bool)
+    for bit in maskbits:
+        mask_clean &= (cat['MASKBITS'] & 2**bit)==0
+    print(np.sum(~mask_clean), np.sum(~mask_clean)/len(mask_clean))
+    mask_quality &= mask_clean
+
+    for magnification in [0.99, 1., 1.01]:
+
+        gmag = 22.5 - 2.5 * np.log10((cat['FLUX_G'] * magnification / cat['MW_TRANSMISSION_G']).clip(1e-7))
+        rmag = 22.5 - 2.5 * np.log10((cat['FLUX_R'] * magnification / cat['MW_TRANSMISSION_R']).clip(1e-7))
+        zmag = 22.5 - 2.5 * np.log10((cat['FLUX_Z'] * magnification / cat['MW_TRANSMISSION_Z']).clip(1e-7))
+        w1mag = 22.5 - 2.5 * np.log10((cat['FLUX_W1'] * magnification / cat['MW_TRANSMISSION_W1']).clip(1e-7))
+
+        if ff_factor:
+            zfibermag = 22.5 - 2.5 * np.log10((cat['FIBERFLUX_Z'] * (1 + (magnification-1) * cat['ff_factor']) / cat['MW_TRANSMISSION_Z']).clip(1e-7))
+        else:
+            zfibermag = 22.5 - 2.5 * np.log10((cat['FIBERFLUX_Z'] * (1 + (magnification-1) * 1               ) / cat['MW_TRANSMISSION_Z']).clip(1e-7))
+
+        if pz_magnification:
+            pz = Table(fitsio.read('/Users/rongpu/Documents/Data/lrg_xcorr/magnification/lrg_magnification_pz_{}_{:g}.fits'.format(field, magnification)))
+        else:
+            pz = Table(fitsio.read('/Users/rongpu/Documents/Data/lrg_xcorr/magnification/lrg_magnification_pz_{}_1.fits'.format(field)))
+        pz = pz[mask_ns]
+
+        if len(pz)!=len(cat):
+            raise ValueError
+
+        mask_lrg = np.full(len(cat), True)
+
+        if field=='south':
+            mask_lrg &= zmag - w1mag > 0.8 * (rmag - zmag) - 0.6  # non-stellar cut
+            mask_lrg &= zfibermag < 21.6                   # faint limit
+            mask_lrg &= (gmag - w1mag > 2.9) | (rmag - w1mag > 1.8)  # low-z cuts
+            mask_lrg &= (
+                ((rmag - w1mag > (w1mag - 17.14) * 1.8)
+                 & (rmag - w1mag > (w1mag - 16.33) * 1.))
+                | (rmag - w1mag > 3.3)
+            )  # double sliding cuts and high-z extension
+        else:
+            mask_lrg &= zmag - w1mag > 0.8 * (rmag - zmag) - 0.6  # non-stellar cut
+            mask_lrg &= zfibermag < 21.61                   # faint limit
+            mask_lrg &= (gmag - w1mag > 2.97) | (rmag - w1mag > 1.8)  # low-z cuts
+            mask_lrg &= (
+                ((rmag - w1mag > (w1mag - 17.13) * 1.83)
+                 & (rmag - w1mag > (w1mag - 16.31) * 1.))
+                | (rmag - w1mag > 3.4)
+            )  # double sliding cuts and high-z extension
+
+        if field=='south':
+            pz_cuts = [0.400, 0.540, 0.713, 0.860, 1.020]
+        else:
+            pz_cuts = [0.400, 0.545, 0.719, 0.851, 1.024]
+
+        for bin_index in range(len(pz_cuts)-1):
+
+            pz_min, pz_max = pz_cuts[bin_index], pz_cuts[bin_index+1]
+            mask_pz = (pz['Z_PHOT_MEDIAN']>=pz_min) & (pz['Z_PHOT_MEDIAN']<pz_max)
+
+            counts['{}_bin_{}_{:.3f}'.format(field, bin_index+1, magnification)] = int(np.sum(mask_quality & mask_lrg & mask_pz))
+
+fn = 'lrg_counts'
+if not pz_magnification:
+    fn += '_no_pz_mag'
+if not ff_factor:
+    fn += '_no_ff'
+
+with open('/Users/rongpu/git/desi-targets/lrg_xcorr/magnification/data/{}.txt'.format(fn), "w") as f:
+    yaml.dump(counts, f)
